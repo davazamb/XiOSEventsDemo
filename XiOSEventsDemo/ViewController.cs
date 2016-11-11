4b08075c38ca10023850cf7cbbaecc1b44459152
David Zambrano <davazamb10@outlook.com>
2016-11-10 21:07:32 -0400

    Demostración de Eventos
    Eventos entre textfield y button.

--- XiOSEventsDemo/ViewController.cs
+++ XiOSEventsDemo/ViewController.cs
@@ -15,12 +15,31 @@ namespace XiOSEventsDemo
 		{
 			base.ViewDidLoad();
 			// Perform any additional setup after loading the view, typically from a nib.
+			btnStart.AccessibilityIdentifier = "myButton";
+			btnStart.TouchUpInside+= BtnStart_TouchUpInside;
+		}
+
+		partial void ValueChanged(UITextField sender)
+		{
+			if (!string.IsNullOrEmpty(txtName.Text))
+				txtPassword.Enabled = true;
 		}
 
 		public override void DidReceiveMemoryWarning()
 		{
 			base.DidReceiveMemoryWarning();
 			// Release any cached data, images, etc that aren't in use.
+		}
+
+		void BtnStart_TouchUpInside(object sender, EventArgs e)
+		{
+			if (!string.IsNullOrEmpty(txtName.Text) && !string.IsNullOrEmpty(txtPassword.Text))
+			{
+				txtCode.Enabled = true;
+				btnStart.TouchUpInside -= BtnStart_TouchUpInside;
+				btnStart.TouchUpInside += (sen, eve) => this.ShowPopup("Hola", "Alerta");
+			}
+			
 		}
 	}
 }
--- XiOSEventsDemo/ViewController.designer.cs
+++ XiOSEventsDemo/ViewController.designer.cs
@@ -1,17 +1,60 @@
-﻿//
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
 
 namespace XiOSEventsDemo
 {
-	[Register("ViewController")]
-	partial class ViewController
-	{
-		void ReleaseDesignerOutlets()
-		{
-		}
-	}
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btnStart { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField txtCode { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField txtName { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField txtPassword { get; set; }
+
+        [Action ("ValueChanged:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void ValueChanged (UIKit.UITextField sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (btnStart != null) {
+                btnStart.Dispose ();
+                btnStart = null;
+            }
+
+            if (txtCode != null) {
+                txtCode.Dispose ();
+                txtCode = null;
+            }
+
+            if (txtName != null) {
+                txtName.Dispose ();
+                txtName = null;
+            }
+
+            if (txtPassword != null) {
+                txtPassword.Dispose ();
+                txtPassword = null;
+            }
+        }
+    }
 }
